37b943fffc5d0d284a7994984e0664f5e82262f3
raahul5770 <raahulrajah@gmail.com>
2023-07-21 18:23:55 -0400

    mqtt testing

--- raspberry_pi_files/mqtt_master_face_recognition.py
+++ raspberry_pi_files/mqtt_master_face_recognition.py
@@ -130,7 +130,7 @@ def on_message(client, userdata, msg):
     elif msg.payload.decode() == 'This is New Face':
         #client.subscribe("test/servo")
 
-        capture = cv2.VideoCapture(0)
+        """capture = cv2.VideoCapture(0)
         capture.set(3, 640) #width
         capture.set(4, 480) #height
 
@@ -275,7 +275,10 @@ def on_message(client, userdata, msg):
 
 
         print("\n [INFO] {0} Faces Trained. Program Exiting".format(len(numpy.unique(ids))))
-        client.publish("test/app", "\n [INFO] {0} Faces Trained. Program Exiting".format(len(numpy.unique(ids))))
+        client.publish("test/app", "\n [INFO] {0} Faces Trained. Program Exiting".format(len(numpy.unique(ids))))"""
+
+        sql_face_data_collection(client)
+        sql_face_trainer(client)
 
 
         
@@ -284,3 +287,149 @@ def on_message(client, userdata, msg):
 
 connect_mqtt()
 
+def sql_face_data_collection(client):
+    capture = cv2.VideoCapture(0)
+    capture.set(3, 640) #width
+    capture.set(4, 480) #height
+
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default1.xml")
+
+
+
+    def changeDB(ID,Name,Photos):
+        conn = sqlite3.connect("FaceBase.db")
+        cursor = conn.execute("SELECT * FROM people WHERE PersonID = ?", (ID,))
+        #cursor.execute(cmd)
+            
+        isRecordExist = cursor.fetchone()
+        #isRecordExist=0
+        #for row in cursor:
+            #isRecordExist=1
+
+
+        try:
+            if(isRecordExist is not None):
+                cmd="UPDATE people SET Name = ?,Photos = ? WHERE PersonID = ?"
+                conn.execute(cmd, (str(Name), Photos, ID))
+            else:
+                cmd="INSERT INTO people(PersonID, Name, Photos) VALUES(?, ?, ?)"	 
+                conn.execute(cmd, (ID, str(Name), Photos))
+
+            #conn.execute(cmd)  
+            conn.commit()
+            print("UPDATED SUCCESFULLY")
+        except:
+            print("ERROR")
+
+        conn.close()
+            
+    id=input('Enter user id : ')
+    name=input('Enter your name : ')
+
+    sampleNum=0 
+
+    print("Please look at the camera. Capturing face samples...")
+    client.publish("test/app","Please look at the camera. Capturing face samples...")
+    photos_list = []
+
+
+    count = 0
+    while(True):
+        ret, image = capture.read()
+        image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE) #Vertical camera flip
+        gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #grayscale
+        faces_detected = face_cascade.detectMultiScale(gray_img, 1.2, 5, minSize = (20,20))
+
+        for (x,y,w,h) in faces_detected:
+            cv2.rectangle(image,(x,y),(x+w, y+h),(255,0,0),2)
+            count += 1
+            photos_list.append(gray_img[y:y+h,x:x+w])
+            #cv2.imwrite("dataset/User." + str(id) + '.' + str(count) + ".jpg", gray_img[y:y+h,x:x+w])
+            cv2.imshow('image', image)
+        
+        a = cv2.waitKey(100) & 0xff
+        if a == 27: #Press escape to quit
+            break
+        elif count >= 5:
+            break
+
+
+    serialized_photos = pickle.dumps(photos_list)
+    changeDB(id,name,serialized_photos)
+
+    print("Samples taken and exiting program")
+    client.publish("test/app", "Samples taken and exiting program")
+    capture.release()
+    cv2.destroyAllWindows()
+
+def sql_face_trainer(client):
+
+    recognize_face = cv2.face.LBPHFaceRecognizer_create()
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default1.xml")
+
+    print("Face training in progress. This may take a few seconds...")
+    client.publish("test/app","Face training in progress. This may take a few seconds...")
+
+    all_photos = []
+
+    def getImagesAndLabels(): 
+        conn=sqlite3.connect("FaceBase.db")
+        cmd="SELECT * FROM people"
+        cursor=conn.execute(cmd)
+
+
+        if cursor.fetchone() is None:
+            print("No rows found in the database.")
+            cursor.close()
+            conn.close()
+            return [],[]
+        
+        cursor.execute("SELECT PersonID, Photos FROM people")
+
+
+        rows = cursor.fetchall()
+
+        face_samples = []
+        face_ids = []
+
+        for row in rows:
+            photo_value = row[1]  # Assuming the column is the first (index 0)
+            id_value = row[0]
+            #print(id_value)
+            photos_list = pickle.loads(photo_value)
+            id_and_photos = []
+            id_and_photos.append(photos_list)
+            id_and_photos.append(id_value)
+            all_photos.append(id_and_photos)
+        #print(len(id_and_photos))
+        #print(len(all_photos))
+        for photos in all_photos:
+            for photo in photos[0]:
+                #PIL_image = Image.open(photo).convert("L")
+                numpy_image = numpy.array(photo, 'uint8')
+
+                faces_detected = face_cascade.detectMultiScale(numpy_image)
+                for (x,y,w,h) in faces_detected:
+                    face_samples.append(numpy_image[y:y+h,x:x+w])
+                    #print(photos[1])
+                    face_ids.append(photos[1])
+
+        cursor.close()
+        conn.close()
+        
+        return face_samples, face_ids
+
+    faces, ids = getImagesAndLabels()
+
+    #print(len(ids))
+    #for id in ids:
+        #print(id)
+    #for face in faces:
+        #print("YOO")
+    recognize_face.train(faces, numpy.array(ids))
+
+    recognize_face.write('trainer_sql/trainer_sql.yml')
+
+
+    print("\n [INFO] {0} Faces Trained. Program Exiting".format(len(numpy.unique(ids))))
+    client.publish("test/app", "\n [INFO] {0} Faces Trained. Program Exiting".format(len(numpy.unique(ids))))
